86dbcdd737c5d144af9a27d71553377d997d1322
Aaron Watters <awatters@simonsfoundation.org>
2021-04-05 12:52:06 -0400

    infrastructure

---
+++ mouse_embryo_labeller/__init__.py
@@ -0,0 +1,2 @@
+
+# required module __init__ file
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+url = ""
+version = "0.1.0"
+readme = open('README.md').read()
+
+setup(
+    name="feedWebGL2",
+    packages=["feedWebGL2"],
+    version=version,
+    description="Tools for implementing WebGL2 feedback processing stages for graphics preprocessing or other purposes",
+    long_description=readme,
+    include_package_data=True,
+    author="Aaron Watters",
+    author_email="awatters@flatironinstitute.org",
+    url=url,
+    install_requires=[
+        "jp_doodle",
+        "jp_proxy_widget",
+        ],
+    # dependancy links doesn't work apparently because you need a tag.
+    #dependency_links=[
+    #    'https://github.com/AaronWatters/jp_doodle/tarball/master',
+    #    ],
+    license="MIT"
+)
