dfdea05ed52b20ebcdf2bb9ab23cf03d714c4df1
Aaron Watters <awatters@simonsfoundation.org>
2021-04-08 08:25:48 -0400

    nucleus assignment etc

--- mouse_embryo_labeller/viz_controller.py
+++ mouse_embryo_labeller/viz_controller.py
@@ -4,6 +4,7 @@ from jp_doodle import array_image
 from jp_doodle.data_tables import widen_notebook
 from jp_doodle import color_chooser
 from mouse_embryo_labeller import nucleus
+import os
 
 
 class VizController:
@@ -24,11 +25,21 @@ class VizController:
     def timestamp(self):
         return self.timestamp_collection.get_timestamp(self.selected_timestamp_id)
 
+    def get_nucleus(self):
+        nid = self.selected_nucleus_id
+        if nid is None:
+            return None
+        else:
+            return self.nucleus_collection.get_nucleus(nid)
+
     def make_widget(self, side=350):
         widen_notebook()
         self.side = side
         ts = self.timestamp()
-        self.info = widgets.HTML(value="Nucleus Labeller Tool")
+        self.info = widgets.HTML(value="Nucleus Labeller Tool.")
+        self.nucleus_info = widgets.HTML(value="Please select or create an nucleus.")
+        info_area1 = widgets.HBox([self.info], layout=widgets.Layout(border='solid'))
+        info_area = widgets.HBox([info_area1, self.nucleus_info])
         self.prev_button = widgets.Button(description="< Prev")
         self.prev_button.on_click(self.go_previous)
         self.timestamp_html = widgets.HTML(value=repr(self.selected_timestamp_id))
@@ -69,6 +80,7 @@ class VizController:
             scale=False,
             hover_text_callback=self.label_image_hover,
         )
+        self.labelled_image_display.image_display.on("click", self.label_image_click)
         image_assembly = widgets.HBox([
             self.labelled_image_display,
             self.raster_display,
@@ -85,6 +97,7 @@ class VizController:
         self.new_button = widgets.Button(description="New", disabled=True)
         self.new_button.on_click(self.new_click)
         self.child_button = widgets.Button(description="Child", disabled=True)
+        self.child_button.on_click(self.child_click)
         self.color_selector = color_chooser.chooser(side=0.5 * side, callback=self.change_color)
         make_nucleus_assembly = widgets.HBox([
             self.nucleus_name_input,
@@ -94,21 +107,74 @@ class VizController:
             #self.nucleus_info,
         ])
         self.left_assambly = widgets.VBox([
-            self.info,
+            info_area,
             top_bar,
             image_assembly,
             make_nucleus_assembly,
         ])
-        self.nucleus_chooser = self.nucleus_collection.create_widget(height=side * 1.5)
+        self.nucleus_chooser = self.nucleus_collection.create_widget(
+            height=side * 1.5,
+            callback=self.set_nucleus_id,
+        )
         self.widget = widgets.HBox([
             self.left_assambly,
             self.nucleus_chooser,
         ])
-        # reset colors
+        # reset colors etc
         self.change_color()
         # fix up buttons, etcetera
         self.redraw()
         return self.widget
+
+    def set_nucleus_id(self, identifier):
+        if self.nucleus_collection.get_nucleus(identifier, check=False):
+            self.selected_nucleus_id = identifier
+        else:
+            self.selected_nucleus_id = None
+        self.show_nucleus_selection()
+
+    def show_nucleus_selection(self):
+        n = self.get_nucleus()
+        info = self.nucleus_info
+        if n is None:
+            info.value = "No nucleus currently selected."
+        else:
+            info.value = '<span style="background-color:%s">NUCLEUS</span> %s' % (n.html_color(), n.identifier)
+
+    def label_image_click(self, event):
+        position = event['model_location']
+        x = int(position["x"])
+        y = int(position["y"])
+        self.info.value = "Click: " + repr((x, y))
+        ts = self.timestamp()
+        extruded = self.extruded_checkbox.value
+        layer = self.layers_slider.value
+        label = ts.get_label(layer, y, x, extruded=extruded)
+        self.info.value = "label " + repr(label)
+        if label == 0:
+            self.info.value = "Cannot assign unlabelled area."
+            return
+        self.assign_nucleus(label)
+
+    def assign_nucleus(self, label):
+        ts = self.timestamp()
+        n = self.get_nucleus()
+        ts.assign_nucleus(label, n)
+        tsid = self.selected_timestamp_id
+        if n is None:
+            self.info.value = "Reset nucleus assignment for label %s in timestamp %s." % (label, tsid)
+        else:
+            self.info.value = "Assigned label %s to nucleus %s in timestamp %s." % (label, n.identifier, tsid)
+        # save the assignment
+        path = self.ts_assignment_path()
+        ts.save_mapping(path)
+        self.redraw()
+
+    def ts_assignment_path(self):
+        tsid = self.selected_timestamp_id
+        assert tsid is not None, "cannot save -- no selected ts"
+        filename = "ts%s.json" % tsid
+        return os.path.join(self.folder, filename)
 
     def label_image_hover(self, x, y, array):
         ts = self.timestamp()
@@ -127,16 +193,25 @@ class VizController:
             raise
         return prefix + suffix
 
-    def new_click(self, button):
+    def child_click(self, button):
+        return self.new_click(button, parent=self.selected_nucleus_id)
+
+    def new_click(self, button, parent_id=None):
         self.info.value = "New clicked."
         identifier = self.nucleus_name_input.value
         color = self.color_selector.color_array
-        parent_id = None
         n = nucleus.Nucleus(identifier, color, parent_id)
         self.nucleus_collection.add_nucleus(n)
         self.nucleus_collection.save_json(self.folder)
-        self.selected_nucleus_id = identifier
+        # only switch to nucleus if no parent
+        if parent_id is None:
+            self.selected_nucleus_id = identifier
         self.nucleus_collection.set_widget_options(callback=None, selected=identifier)
+        self.show_nucleus_selection()
+        self.nucleus_name_input.value = ""
+        self.new_button.disabled = True
+        self.child_button.disabled = True
+        self.color_selector.reset_color_choice()
 
     def nucleus_name_change(self, change):
         self.color_selector.reset_color_choice()
@@ -174,6 +249,7 @@ class VizController:
 
     def redraw(self):
         #tsc = self.timestamp_collection
+        self.show_nucleus_selection()
         tsid = self.selected_timestamp_id
         self.timestamp_html.value = repr(tsid)
         (prv, nxt) = self.previous_next()
