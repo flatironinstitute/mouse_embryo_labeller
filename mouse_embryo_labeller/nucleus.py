cb46fca4a380d1f557604fa3c19fa475b2c64c92
Aaron Watters <awatters@simonsfoundation.org>
2021-05-25 16:35:15 -0400

    draw operations for nucleus

--- mouse_embryo_labeller/nucleus.py
+++ mouse_embryo_labeller/nucleus.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import ipywidgets as widgets
+from mouse_embryo_labeller.color_list import rgbhtml
 
 class Nucleus:
 
@@ -12,6 +13,37 @@ class Nucleus:
         self.parent_id = parent_id
         self.color = np.zeros((3,), dtype=np.int)
         self.color[:] = color
+        self.children = None
+        self.width = None
+        self.position = None
+        self.last_descendent_position = None
+        self.timestamp_indices = set()
+
+    def draw_rectangles(self, on_frame):
+        color = self.html_color()
+        position = self.position
+        for index in self.timestamp_indices:
+            on_frame.frame_rect(x=index, y=position+0.25, h=0.5, w=0.8, color=color)
+
+    def draw_label(self, on_frame):
+        color = self.html_color()
+        position = self.position
+        index = min(self.timestamp_indices)
+        on_frame.text(index-0.1, position+0.5, str(self.identifier) + " ", align="right", valign="center", color=color, background="#eee")
+
+    def draw_link(self, on_frame, nucleus_collection):
+        parent_id = self.parent_id
+        if parent_id is not None:
+            parent = nucleus_collection.get_nucleus(parent_id)
+            color = self.html_color()
+            position = self.position
+            index = min(self.timestamp_indices)
+            p_position = parent.position
+            p_index = min(parent.timestamp_indices)
+            on_frame.line(index+0.5, position+0.5, p_index+0.5, p_position+0.5, color=color, lineWidth=5)
+
+    def add_timestamp_index(self, index):
+        self.timestamp_indices.add(index)
 
     def html_color(self):
         return "rgb(%s,%s,%s)" % tuple(self.color)
